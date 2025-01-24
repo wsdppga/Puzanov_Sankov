9f7df62a94a60cdb6537055bae8409218fe56ac4
wsdppga <168220535+wsdppga@users.noreply.github.com>
2025-01-24 18:45:49 +0300

    Add files via upload

---
+++ Пузанов_Саньков.cs
@@ -0,0 +1,22 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите число a: ");
+        int a = Convert.ToInt32(Console.ReadLine());
+        
+        Console.Write("Введите число b: ");
+        int b = Convert.ToInt32(Console.ReadLine());
+
+        if (b % a == 0 || a % b == 0)
+        {
+            Console.WriteLine("Да, одно из чисел является делителем другого");
+        }
+        else
+        {
+            Console.WriteLine("Нет, ни одно из чисел не является делителем другого");
+        }
+    }
+}
